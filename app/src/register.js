a32b4800425ec659fd4fc51300b999c2810f7849
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-15 12:34:22 +0200

    (chore) Wops forgot to rename the placeholder value

--- app/src/register.js
+++ app/src/register.js
@@ -43,14 +43,14 @@ register.view = (ctrl) => {
         value: register.model.teamOne.player[0](),
         onchange: m.withAttr('value', register.model.teamOne.player[0]),
         class: 'inputPlayer',
-        placeholder: 'Player 3'
+        placeholder: 'Player 1'
       }),
       m('span', { class: 'playerSeperator' }, ' and '),
       m('input', {
         value: register.model.teamOne.player[1](),
         onchange: m.withAttr('value', register.model.teamOne.player[1]),
         class: 'inputPlayer',
-        placeholder: 'Player 4'
+        placeholder: 'Player 2'
       }),
       m('h2', { class: ['heading2__lessVSpace'] }, 'vs.'),
       m('input', {
