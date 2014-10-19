dde26340b05ece5bda9009ec3aadee111e5814d2
Ken Grønnbeck <kengroenn@gmail.com>
2014-10-19 16:14:55 +0200

    (ui) Better responsive design for scoreboard

--- app/src/scoreboard.js
+++ app/src/scoreboard.js
@@ -12,21 +12,21 @@ board.view = (ctrl) => {
   return [
     m('h1', { class: 'heading1' }, 'Scoreboard'),
     m('div', { class: 'scoreboardHeading'}, [
-      m('div', { class: 'teamName_entry'}, 'Team'),
-      m('div', { class: 'games'}, 'Games'),
-      m('div', { class: 'wins'}, 'W'),
-      m('div', { class: 'losses'}, 'L'),
-      m('div', { class: 'draws'}, 'D'),
-      m('div', { class: 'points'}, 'Points')
+      m('div.entryCol.entryHeader', { class: 'teamName_entry'}, 'Team'),
+      m('div.entryCol.entryHeader.games', ''),
+      m('div.entryCol.entryHeader.wins', ''),
+      m('div.entryCol.entryHeader.losses', ''),
+      m('div.entryCol.entryHeader.draws', ''),
+      m('div.entryCol.entryHeader.points', '')
     ]),
     ctrl.scoreboard().map((entry) => {
       return m('div', { class: 'scoreboardEntry' }, [
-        m('div', { class: 'teamName_entry'}, entry.teamName),
-        m('div', { class: 'games'}, entry.games),
-        m('div', { class: 'wins'}, entry.wins),
-        m('div', { class: 'losses'}, entry.loss),
-        m('div', { class: 'draws'}, entry.draws),
-        m('div', { class: 'points'}, entry.points)
+        m('div.entryCol', { class: 'teamName_entry'}, entry.teamName),
+        m('div.entryCol', { class: 'games'}, entry.games),
+        m('div.entryCol', { class: 'wins'}, entry.wins),
+        m('div.entryCol', { class: 'losses'}, entry.loss),
+        m('div.entryCol', { class: 'draws'}, entry.draws),
+        m('div.entryCol', { class: 'points'}, entry.points)
       ])
     })
   ]
