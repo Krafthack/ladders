18126130afac592c8f9d283c867b68e59e157397
Ken Grønnbeck <kengroenn@gmail.com>
2014-11-22 13:26:02 +0100

    (bug) Must use the old model in the migration script

--- scripts/03-match-flatten-teams.js
+++ scripts/03-match-flatten-teams.js
@@ -1,9 +1,30 @@
-var mongoose = require('mongoose');
+var mongoose = require('../api/mongoose-wrapper');
 var Q = require('q');
 var _ = require('lodash');
-var Match = require('../build/model/match');
 
-Match.all().then(function(matches) {
+var Model = mongoose.model('Match', {
+  teams: Array,
+  score: [Number],
+  invalid: Boolean,
+  date: { type: Date, default: Date.now }
+});
+
+var all = function() {
+  var deferred = Q.defer();
+  Model.find({})
+    .sort({date: 'desc'})
+    .exec(function(err, results) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        deferred.resolve(results);
+      }
+    });
+  return deferred.promise;
+}
+
+all().then(function(matches) {
+  console.log(matches.length)
   matches.map(function(match) {
       var teams = _.flatten(match.teams);
       match.teams = teams;
