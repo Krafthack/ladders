1c1b42503229873a57d26c9a0d4fdd60aeedd22b
Ken Grønnbeck <kengroenn@gmail.com>
2014-11-23 12:37:01 +0100

    (bug) Ah. Register was moved remember?

--- api/modules/api.js
+++ api/modules/api.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
-var register = require('./register');
 var matches = require('./matches');
 var scoreboard = require('./scoreboard');
 var player = require('./player');
 
-app.use(register);
 app.use('/api/matches', matches);
 app.use('/api/player', player);
 app.use(scoreboard);
